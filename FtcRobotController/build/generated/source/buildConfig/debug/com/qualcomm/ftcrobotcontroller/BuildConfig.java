b90ff103ab21b7b33d532d21073a282f7fd98f16
Varunv20 <varun.viges@icloud.com>
2023-05-17 09:36:59 -0500

    tng

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2023-04-06T11:29:37.034-0500";
+  public static final String APP_BUILD_TIME = "2023-05-17T09:35:03.246-0500";
 }
